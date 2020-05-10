e6765ad88218535d9ada8b35eca76a40b151c541
JackyJhb <32191103@qq.com>
2020-05-10 16:18:40 +0800

    Change magic ID.

--- User/sccf.h
+++ User/sccf.h
@@ -13,7 +13,7 @@ The meaning of sccf is System Control Configuration File.
 
 #define AT24C128
 #define INIT_KEY        0x3333
-#define INIT_KEY_FLASH  0x2222
+#define INIT_KEY_FLASH  0x2233
 
 #define ADDR_RTD_FILE   0x0000      //Real time data
 #define ADDR_CFG_FILE   0x0400      //Config file data
